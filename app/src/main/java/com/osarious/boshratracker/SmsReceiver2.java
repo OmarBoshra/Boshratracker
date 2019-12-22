8132f90b7e8443c57e138064a3b99332e2ed5a52
OmarBoshra <osarious2@gmail.com>
2019-12-22 23:35:51 +0200

    legit release

---
+++ app/src/main/java/com/osarious/boshratracker/SmsReceiver2.java
@@ -0,0 +1,33 @@
+package com.osarious.boshratracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public  class SmsReceiver2 extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+
+
+    }
+}
+
+
+
+
+
